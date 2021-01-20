915702893a39763f55c251192bbb47631887b170
yangs <856063>
2021-01-20 15:16:28 +0800

    基本工程提交

---
+++ src/main/java/com/yx/marketlibrary/MarketLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.yx.marketlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yx/marketlibrary/MarketLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yx.marketlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
